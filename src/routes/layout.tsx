3863b72f6c80e63043b0adc5ed20dbbc92090bf5
Matthew Harvey <matthewtlharvey@gmail.com>
2023-05-06 19:13:48 +0100

    flowbite js fix

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -13,7 +13,7 @@ export default component$(() => {
   //useStyles$(styles);
   return (
     <>
-      <script src='../../node_modules/flowbite/dist/flowbite' />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" />
       <main>
         <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
           <span class="sr-only">Open sidebar</span>
