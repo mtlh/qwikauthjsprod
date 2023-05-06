509170fbeb51a965200dbb11bc597b9a68c6915f
Matthew Harvey <matthewtlharvey@gmail.com>
2023-05-06 18:59:15 +0100

    sidebar test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui"), require('flowbite/plugin')],
 };
