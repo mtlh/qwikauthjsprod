bd00c0957c973302446b609cd92bf465d5b5359c
Matthew Harvey <matthewtlharvey@gmail.com>
2023-05-08 12:05:47 +0100

    plugin testing auth secret error in prod

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -4,13 +4,13 @@ import type { Provider } from '@auth/core/providers';
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(
   () => ({
-    secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
-    trustHost: true,
     providers: [
       GitHub({
         clientId: process.env.NEXT_PUBLIC_GITHUB_ID!,
         clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET!,
-      }),
+      })
     ] as Provider[],
+    secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+    trustHost: true
   })
 );
