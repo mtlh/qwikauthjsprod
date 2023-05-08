e00ac5b99986ffe98b983c47a9ad9ce3126eb975
Matthew Harvey <matthewtlharvey@gmail.com>
2023-05-08 02:11:51 +0100

    env.get replaced with process.env

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -3,13 +3,13 @@ import GitHub from '@auth/core/providers/github';
 import type { Provider } from '@auth/core/providers';
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(
-  ({ env }) => ({
-    secret: env.get('AUTH_SECRET'),
+  () => ({
+    secret: process.env.AUTH_SECRET,
     trustHost: true,
     providers: [
       GitHub({
-        clientId: env.get('GITHUB_ID')!,
-        clientSecret: env.get('GITHUB_SECRET')!,
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
       }),
     ] as Provider[],
   })
