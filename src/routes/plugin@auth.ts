4483411fa2db3c5770da4e6a61df5d634bd197bd
Matthew Harvey <matthewtlharvey@gmail.com>
2023-05-08 10:36:19 +0100

    auth secret env

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -3,13 +3,13 @@ import GitHub from '@auth/core/providers/github';
 import type { Provider } from '@auth/core/providers';
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(
-  ({env}) => ({
-    secret: env.get("NEXT_PUBLIC_AUTH_SECRET"),
+  () => ({
+    secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
     trustHost: true,
     providers: [
       GitHub({
-        clientId: env.get("NEXT_PUBLIC_GITHUB_ID")!,
-        clientSecret: env.get("NEXT_PUBLIC_GITHUB_SECRET")!,
+        clientId: process.env.NEXT_PUBLIC_GITHUB_ID!,
+        clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET!,
       }),
     ] as Provider[],
   })
